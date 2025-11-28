eac563ef5f4f2d5b45dbb81ee6f52daa404fb18c
Tarun Bevara <tarunbevara10@gmail.com>
2025-11-27 23:55:36 -0500

    141 code

---
+++ 141_linked_list_cycle.py
@@ -0,0 +1,13 @@
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next          # move 1 step
+            fast = fast.next.next     # move 2 steps
+
+            if slow == fast:          # pointers meet → cycle exists
+                return True
+
+        return False                   # fast reached null → no cycle
