0d5a2c48e88b7c0f24395fc3b9d94b710ff97579
TarunBevara10 <tarunbevara10@gmail.com>
2025-12-11 14:09:12 -0500

    103 sol

---
+++ 103_Binary_Zigzag_Traversal.py
@@ -0,0 +1,43 @@
+from collections import deque
+from typing import Optional, List
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        
+        result = []
+        queue = deque([root])
+        # Flag to track direction: True = Left->Right, False = Right->Left
+        left_to_right = True
+        
+        while queue:
+            level_size = len(queue)
+            current_level = []
+            
+            for _ in range(level_size):
+                node = queue.popleft()
+                current_level.append(node.val)
+                
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            
+            # If the current direction is Right -> Left, reverse the list
+            if not left_to_right:
+                result.append(current_level[::-1])
+            else:
+                result.append(current_level)
+            
+            # Flip the direction for the next level
+            left_to_right = not left_to_right
+            
+        return result
