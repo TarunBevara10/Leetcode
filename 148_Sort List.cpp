38660b226388b8e3d4111fafe7111c0c43185bcd
TarunBevara10 <tarunbevara10@gmail.com>
2026-01-06 19:52:24 -0500

    148 sol

---
+++ 148_Sort List.cpp
@@ -0,0 +1,63 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ * int val;
+ * ListNode *next;
+ * ListNode() : val(0), next(nullptr) {}
+ * ListNode(int x) : val(x), next(nullptr) {}
+ * ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        // Base case: 0 or 1 nodes
+        if (!head || !head->next) return head;
+        
+        // 1. Split the list into two halves
+        ListNode* mid = getMid(head);
+        ListNode* left = head;
+        ListNode* right = mid->next;
+        mid->next = nullptr; // Break the link
+        
+        // 2. Recursively sort each half
+        left = sortList(left);
+        right = sortList(right);
+        
+        // 3. Merge the sorted halves
+        return merge(left, right);
+    }
+
+private:
+    ListNode* getMid(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head->next; // Start fast ahead to find the end of the first half
+        
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+    
+    ListNode* merge(ListNode* list1, ListNode* list2) {
+        ListNode dummy(0);
+        ListNode* tail = &dummy;
+        
+        while (list1 && list2) {
+            if (list1->val < list2->val) {
+                tail->next = list1;
+                list1 = list1->next;
+            } else {
+                tail->next = list2;
+                list2 = list2->next;
+            }
+            tail = tail->next;
+        }
+        
+        // Attach remaining nodes
+        tail->next = (list1) ? list1 : list2;
+        
+        return dummy.next;
+    }
+};
